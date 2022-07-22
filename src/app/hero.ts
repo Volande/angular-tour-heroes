92534f6edbecded7cfcb40f86795363c6a7caa56
Volande <vladimirmoskalov0@gmail.com>
2022-07-22 16:22:54 +0300

    check

---
+++ src/app/hero.ts
@@ -0,0 +1,8 @@
+export interface Hero {
+  id: number;
+  name: string;
+  img: string[];
+  size: string[];
+  price: string;
+  content: string;
+}
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,51 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] =[
+  {id: 1,
+    name: 'Нежное женственное платье из креп -шифона',
+    img: ["./assets/photo/photo1.jpg",
+      "./assets/photo/photo2.jpg" ],
+    size: ["X","L"],
+    content:'Топ можно носить в двух вариантах , завязывая на груди ,или на запах <br>Костюм выполнен из натурального итальянского льна , в жаркий день в нем Вы будите чувствовать себя максимально комфортно',
+    price:''
+  },
+  {id: 2,
+    name: 'Платье -бюстье из натурального итальянского льна',
+    img: ["./assets/photo/photo3.jpg",
+      "./assets/photo/photo4.jpg" ],
+    size: ["X","L"],
+    content:'',
+    price:''
+  },
+
+  {id: 3,
+    name: 'Костюм топ с юбкой из итальянского льна',
+    img: ["./assets/photo/photo5.jpg",
+      "./assets/photo/photo6.jpg" ],
+    size: ["X","L"],
+    content:'',
+    price:''
+  },
+  {id: 4,
+    name: 'Платье из итальянского полированного хлопка с шёлком',
+    img: ["./assets/photo/photo7.jpg",
+      "./assets/photo/photo8.jpg"],
+    size: ["X","L"],
+    content:'',
+    price:''},
+  {id: 5,
+    name: 'Сочное платье в оттенке Сицилийский апельсин',
+    img: ["../assets/photo/photo9.jpg",
+      "../assets/photo/photo10.jpg"],
+    size: ["X","L"],
+    content:'',
+    price:''},
+  {id: 6,
+    name: 'Костюм топ с юбкой из итальянского льна',
+    img: ["./assets/photo/photo11.jpg",
+      "./assets/photo/photo12.jpg"],
+    size: ["X","L"],
+    content:'',
+    price:''},
+];
+
